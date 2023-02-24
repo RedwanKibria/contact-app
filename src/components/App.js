e1507de75ed4d0496aac4cb683dd0eefc1463700
RedwanKibria <86778079+RedwanKibria@users.noreply.github.com>
2023-02-24 22:55:43 +0600

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
+import React, {useState} from 'react';
 import './App.css';
+import Header from "./header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
 
 function App() {
+  const [contacts, setContacts] = useState([]);
   return (
-    <div>Hello world</div>
+    <div className='ui container'>
+      <Header />
+      <AddContact />
+      <ContactList contacts={contacts} />
+    </div>
   );
 }
 
